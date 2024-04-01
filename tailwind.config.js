ffe52a5328b3f79ecac16fb7efa2f7e6b9c1966d
acz19 <acz1992@hotmail.co.uk>
2024-04-01 16:07:39 +0200

    Configure TailWind custom classes and themes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,35 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+	theme: {
+		/*  fontFamily: {
+			primary: "",
+			secondary: "",
+			tertiary: "", */
+		screens: {
+			tablet: "640px",
+			// => @media (min-width: 640px)
+			// { ... }
 
+			laptop: "1024px",
+			// => @media (min-width: 1024px)
+			// { ... }
+
+			desktop: "1280px",
+			// => @media (min-width: 1280px)
+			// { ... }
+
+			wideDesktop: "1440px",
+			// => @media (min-width: 1440px)
+			// { ... }
+		},
+		extend: {
+			/* colors: {
+				primary: "",
+				secondary: "",
+				background: "",
+    }, */
+		},
+		plugins: [],
+	},
+};
