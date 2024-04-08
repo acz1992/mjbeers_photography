df7877644624e02f5c9223e956e7af4fd50a741f
acz19 <acz1992@hotmail.co.uk>
2024-04-08 12:00:14 +0100

    Delete files

--- src/components/MobileNav.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const MobileNav = () => {
-	return <></>;
-};
-
-export default MobileNav;
--- src/components/PhotoTile.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const PhotoTile = () => {
-	return <div>Photo Tile</div>;
-};
-
-export default PhotoTile;
