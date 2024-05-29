0b6bb48e1183accda588fbc08398b4b31df6e44c
acz19 <acz1992@hotmail.co.uk>
2024-05-29 12:02:51 +0200

    Deleted unnecessary data folder

--- frontend/src/data/photo.ts
+++
@@ -1,9 +0,0 @@
-/* import { client } from "../client";
-
-export async function getPhotos() {
-	const query =
-		'*[_type == "photo"]{_id, title, description, category, "imageAddress": image.asset->url}';
-	const photos = await client.fetch(query);
-	return photos;
-}
- */
